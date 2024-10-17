a5ed72cf2fb3baff096628b67584a20ae2e7bc48
Nich0l3 <146418190+Nich0l3@users.noreply.github.com>
2024-10-18 04:08:31 +0530

    backend
    begin backend dev for running classifier algorithm

--- iot/rpi/dep/linux-dep.sh
+++ iot/rpi/dep/linux-dep.sh
@@ -1,6 +1,10 @@
 sudo apt install \
 mosquitto mosquitto-clients avahi-daemon avahi-utils libnss-mdns \
+gunicorn nginx\
 -y
 
 sudo cp dep/device-conf/avahi-daemon.conf /etc/avahi/ 
 sudo cp dep/device-conf/mqtt.service /etc/avahi/services/  
+
+echo allow_anonymous true >> /etc/mosquitto/mosquitto.conf
+echo listener 1883 >> /etc/mosquitto/mosquitto.conf
