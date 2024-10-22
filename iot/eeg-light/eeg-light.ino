69784f6b888b946ea634dc7c9ce0111fee3450a1
Naman Jain <114279102+LoneBub@users.noreply.github.com>
2024-10-22 10:50:27 +0530

    Update eeg-light.ino

--- iot/eeg-light/eeg-light.ino
+++ iot/eeg-light/eeg-light.ino
@@ -1,28 +1,30 @@
 #include <PubSubClient.h>
 //#include <ESPmDNS.h>
-#include "cred.h"
+//#include "cred.h"
 #include <ArduinoJson.h>
 
+#define MY_SSID "Modify"
+#define THR -40
 
 #define ESP8266
 
 #ifdef ESP32
-  #include <WiFi.h>
+#include <WiFi.h>
 #endif
 #ifdef ESP8266
-  #include <ESP8266WiFi.h>
-  #define LOW             1
-  #define HIGH            0
+#include <ESP8266WiFi.h>
+#define LOW 1
+#define HIGH 0
 #endif
 
-#define DEVID		        "esp1"
+#define DEVID "esp1"
 
 // WiFi settings
-const char *ssid      = WIFI_SSID;        
-const char *password  = WIFI_PASSWORD;    
+const char* ssid = "Modify";
+const char* password = "isuq5478";
 
 // MQTT Broker settings
-const char* mqtt_server = "192.168.1.36";
+const char* mqtt_server = "192.168.1.145";
 const int mqtt_port = 1883;
 WiFiClient espClient;
 PubSubClient mqtt_client(espClient);
@@ -33,77 +35,115 @@ const char* mqtt_topic_1 = "esp1/fan";
 const char* device_topic = "esp/devices";
 
 
+int sig_strength;
+
+
 void connectToWiFi() {
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    Serial.println("Checking WiFi signal 1...");
+
+    int n = WiFi.scanNetworks();
+    for (int i = 0; i < n; ++i) {
+      if (WiFi.SSID(i) == MY_SSID && WiFi.RSSI(i) > THR) {
+        WiFi.begin(ssid, password);
+        Serial.println("Connecting to WiFi");
+        Serial.print(WiFi.SSID(i));
+        Serial.print(" (");
+        Serial.print(WiFi.RSSI(i));
+        Serial.println(")");
+        Serial.println();
+        while (WiFi.status() != WL_CONNECTED) {
+          Serial.print('.');
+          delay(1000);
+        }
+        break;
+      }
+    }
   }
+  publishArray();
   Serial.println("\nConnected to the WiFi network");
 }
 
-void subscribeToTopics(){
+void disconnectFromWiFi() {
+  Serial.println("Checking WiFi signal 2...");
+  int n = WiFi.scanNetworks();
+  for (int i = 0; i < n; ++i) {
+    if (WiFi.SSID(i) == MY_SSID && WiFi.RSSI(i) < THR) {
+      Serial.println("WiFi signal strength is low. Disconnecting...");
+      WiFi.disconnect();
+      Serial.println("Disconnected from WiFi.");
+      break;
+    }
+  }
+}
+
+
+void subscribeToTopics() {
   mqtt_client.subscribe(mqtt_topic_0);
   mqtt_client.subscribe(mqtt_topic_1);
   mqtt_client.subscribe(device_topic);
 }
 
 void connectToMQTTBroker() {
-    while (!mqtt_client.connected()) {
-        String client_id = "esp8266-client-" + String(WiFi.macAddress());
-        Serial.printf("Connecting to MQTT Broker as %s.....\n", client_id.c_str());
-        if (mqtt_client.connect(client_id.c_str())) {
-            Serial.println("Connected to MQTT broker");
-            
-            subscribeToTopics();
+  while (!mqtt_client.connected()) {
 
-        } else {
-            Serial.print("Failed to connect to MQTT broker, rc=");
-            Serial.print(mqtt_client.state());
-            Serial.println(" try again in 5 seconds");
-            delay(5000);
-        }
+    while (WiFi.status() != WL_CONNECTED) {
+      connectToWiFi();
     }
-}
 
-void set_state(uint8_t state){
-  digitalWrite(LED_BUILTIN,state);
-//  digitalWrite(D6,state);
-//  digitalWrite(D7,state);
+    String client_id = "esp8266-client-" + String(WiFi.macAddress());
+    Serial.printf("Connecting to MQTT Broker as %s.....\n", client_id.c_str());
+    if (mqtt_client.connect(client_id.c_str())) {
+      Serial.println("Connected to MQTT broker");
+      subscribeToTopics();
+
+    } else 
+       //break;
+    {
+      Serial.print("Failed to connect to MQTT broker, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+// int LED_BUILTIN = 2;
+void set_state(uint8_t state) {
+  digitalWrite(LED_BUILTIN, state);
 }
 
 String messageTemp = "";
 
-void mqttCallback(char *topic, byte *payload, unsigned int length) {
-    Serial.print("Message received on topic: ");
-    Serial.println(topic);
-    Serial.print("Message:");
-    char messageTemp;
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message received on topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  char messageTemp;
 
-    for (unsigned int i = 0; i < length; i++) {
-        messageTemp += (char)payload[i];
-        Serial.print(messageTemp);
-        Serial.println();
-    }
-    
-    // if ((String)topic == mqtt_topic_0){
-      if(messageTemp == '0'){
-          Serial.println("switched to LOW");
-          set_state(LOW);
-
-      } else if (messageTemp == '1'){
-          Serial.println("switched to HIGH");    
-          set_state(HIGH);
-      }
-    // }
+  for (unsigned int i = 0; i < length; i++) {
+    messageTemp += (char)payload[i];
+    Serial.print(messageTemp);
     Serial.println();
-    Serial.println("-----------------------");
+  }
+
+  // if ((String)topic == mqtt_topic_0){
+  if (messageTemp == '0') {
+    Serial.println("switched to LOW");
+    set_state(LOW);
+
+  } else if (messageTemp == '1') {
+    Serial.println("switched to HIGH");
+    set_state(HIGH);
+  }
+  // }
+  Serial.println();
+  Serial.println("-----------------------");
 }
 
 void publishArray() {
   // Array to send
-  String arrayData[] = {"light", "fan"};
+  String arrayData[] = { "light", "fan" };
   const int arraySize = sizeof(arrayData) / sizeof(arrayData[0]);
 
   // Create a JSON document
@@ -123,29 +163,27 @@ void publishArray() {
   mqtt_client.publish(device_topic, jsonBuffer);
 }
 
+
+
 void setup() {
-    Serial.begin(115200);
-    
-    pinMode(LED_BUILTIN,OUTPUT);
- //   pinMode(D6,OUTPUT);
- //   pinMode(D7,OUTPUT);
-    set_state(LOW);
-    
-    connectToWiFi();
-    
-//    startmDNS();
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  //   pinMode(D6,OUTPUT);
+  //   pinMode(D7,OUTPUT);
+  set_state(LOW);
 
-    mqtt_client.setServer(mqtt_server, mqtt_port);
-    mqtt_client.setCallback(mqttCallback);
-    
-    connectToMQTTBroker();
-    publishArray();
+  //    startmDNS();
 
+  mqtt_client.setServer(mqtt_server, mqtt_port);
+  mqtt_client.setCallback(mqttCallback);
+
+  connectToMQTTBroker();
 }
 
 void loop() {
-    if (!mqtt_client.connected()) {
-        connectToMQTTBroker();
-    }
-    mqtt_client.loop();
+  if (!mqtt_client.connected()) {
+    connectToMQTTBroker();
+  }
+  mqtt_client.loop();
+  disconnectFromWiFi();
 }
