149ee729acdc68d895d87f04467842d23460e6fe
Nich0l3 <146418190+Nich0l3@users.noreply.github.com>
2024-11-25 05:27:34 +0530

    rpi setup

---
+++ iot/rpi/dep/.bash_aliases
@@ -0,0 +1,7 @@
+#custom aliases
+alias up='sudo apt update -y && sudo apt upgrade -y'
+alias x='xclip -sel clipboard'
+
+#source
+source ~/.env/bin/activate
+
--- iot/rpi/dep/linux-dep.sh
+++ iot/rpi/dep/linux-dep.sh
@@ -1,10 +1,11 @@
-sudo apt install \
-mosquitto mosquitto-clients avahi-daemon avahi-utils libnss-mdns \
-gunicorn nginx\
+sudo apt install neovim xclip\
+ mosquitto mosquitto-clients avahi-daemon avahi-utils libnss-mdns \
+ gunicorn nginx\
  -y
 
 sudo cp device-conf/avahi-daemon.conf /etc/avahi/ 
 sudo cp device-conf/mqtt.service /etc/avahi/services/  
+cp device-conf/.bash_aliases ~/
 
 echo allow_anonymous true >> /etc/mosquitto/mosquitto.conf
 echo listener 1883 >> /etc/mosquitto/mosquitto.conf
